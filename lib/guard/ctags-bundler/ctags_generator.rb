b312c7833bd42b35886f1fefb245acf324c7ece3
Gustavo Barron <cicloid@42fu.com>
2012-07-31 12:54:15 -0500

    Added custom_path to opts

--- lib/guard/ctags-bundler/ctags_generator.rb
+++ lib/guard/ctags-bundler/ctags_generator.rb
@@ -6,14 +6,14 @@ module Guard
       end
 
       def generate_project_tags
-        generate_tags(@opts[:src_path] || ".", "tags")
+        generate_tags(@opts[:src_path] || ".", custom_path_for("tags"))
       end
 
       def generate_bundler_tags
         definition = Bundler::Definition.build("Gemfile", "Gemfile.lock", nil)
         runtime = Bundler::Runtime.new(Dir.pwd, definition)
         paths = runtime.requested_specs.map(&:full_gem_path)
-        generate_tags(paths, "gems.tags")
+        generate_tags(paths, custom_path_for("gems.tags") )
       end
 
       private
@@ -22,12 +22,21 @@ module Guard
         if path.instance_of?(Array)
           path = path.join(' ').strip
         end
-
+        system("mkdir  ./#{@opts[:custom_path]}") if @opts[:custom_path]
         cmd = "find #{path} -type f -name \\*.rb | ctags -f #{tag_file} -L -"
         cmd << " -e" if @opts[:emacs]
         system(cmd)
-        system("cat tags gems.tags > TAGS") if @opts[:emacs]
+        system("cat #{custom_path_for("tags")} #{custom_path_for("gems.tags")} > TAGS") if @opts[:emacs]
       end
+
+      def custom_path_for(file)
+        if @opts[:custom_path]
+          return "./#{@opts[:custom_path]}/#{file}"
+        else
+          return file
+        end
+      end
+
     end
   end
 end
